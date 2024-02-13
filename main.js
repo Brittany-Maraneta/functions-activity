a181ee287dc51e092a1dad7f3a4a013746634673
Brittany Maraneta <156222689+Brittany-Maraneta@users.noreply.github.com>
2024-02-13 12:37:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var greetingBttn = document.getElementById("greetingBttn");
+  let greetingBttn = document.getElementById("greetingBttn");
 
   // adding an event for the button
   greetingBttn.addEventListener("click", function () {
-    var newGreet = prompt("New greeting:");
+    let newGreet = prompt("New greeting:");
 
     // Updating the greeting once you enter something in the box
     if (newGreet !== null && newGreet !== "") {
